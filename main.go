214d6edfb6f5c6df41d14baa10d3d5fda19ec37f
Andy Spohn <Andy@AndySpohn.com>
2015-11-08 09:53:27 -0500

    Remove test print lines

--- main.go
+++ main.go
@@ -17,20 +17,4 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// https://groups.google.com/forum/embed/#!topic/golang-nuts/Usu-B5rcCJs
 
 	fmt.Fprint(w, "<html><head/><body>Hello, world!<br /><br /></body></html>")
-
-	ip, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err != nil {
-		//return nil, fmt.Errorf("userip: %q is not IP:port", req.RemoteAddr)
-
-		fmt.Fprintf(w, "userip: %q is not IP:port", r.RemoteAddr)
-	}
-
-	userIP := net.ParseIP(ip)
-	// if userIP == nil {
-	// 	//return nil, fmt.Errorf("userip: %q is not IP:port", req.RemoteAddr)
-	// 	fmt.Fprintf(w, "userip: %q is not IP:port", r.RemoteAddr)
-	// 	return
-	// }
-
-	fmt.Fprint(w, userIP)
 }
