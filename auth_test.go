b7dad93a482e35454bcf91f2174640736b7b4c15
Andy Spohn <Andy@AndySpohn.com>
2015-11-08 08:49:36 -0500

    Simplify test data table

--- auth_test.go
+++ auth_test.go
@@ -11,13 +11,13 @@ type item struct {
 }
 
 var testdata = []item{
-	item{"192.168.1.67", "192.168.1.0/24", true},
-	item{"192.168.1.67", "192.168.1.0/28", false},
-	item{"192.168.1.67", "0.0.0.0/0", true},
-	item{"not_an_ip", "0.0.0.0/0", false},
-	item{"192.168.1.67", "not_a_cidr", false},
-	item{"", "0.0.0.0/0", false},
-	item{"192.168.1.67", "", false},
+	{"192.168.1.67", "192.168.1.0/24", true},
+	{"192.168.1.67", "192.168.1.0/28", false},
+	{"192.168.1.67", "0.0.0.0/0", true},
+	{"not_an_ip", "0.0.0.0/0", false},
+	{"192.168.1.67", "not_a_cidr", false},
+	{"", "0.0.0.0/0", false},
+	{"192.168.1.67", "", false},
 }
 
 func TestCIDRMatch(t *testing.T) {
