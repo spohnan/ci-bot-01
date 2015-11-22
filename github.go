8ee1b05fb91a312b1f742baf241ad4169b1af5ef
Andy Spohn <Andy@AndySpohn.com>
2015-11-22 13:41:37 -0500

    Remove dev logging

--- github.go
+++ github.go
@@ -8,17 +8,11 @@ import (
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 	"google.golang.org/appengine"
-	"google.golang.org/appengine/log"
 )
 
 func rejectIssue(r *http.Request, ie github.IssueActivityEvent) {
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("CI_BOT_GITHUB_KEY")})
 	tc := oauth2.NewClient(appengine.NewContext(r), ts)
 	client := github.NewClient(tc)
-
-	log.Infof(c, "Name %s Num %d", *ie.Repo.Name, *ie.Issue.Number)
-	_, _, err := client.Issues.AddLabelsToIssue("spohnan", *ie.Repo.Name, *ie.Issue.Number, []string{"wontfix"})
-	if err != nil {
-		log.Infof(c, "%s", err)
-	}
+	client.Issues.AddLabelsToIssue("spohnan", *ie.Repo.Name, *ie.Issue.Number, []string{"wontfix"})
 }
